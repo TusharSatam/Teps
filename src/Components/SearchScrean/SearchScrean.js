f3903dd25b5154de7c357a07440c4306c7e087a6
kamruz-zzaman <kamruz.zaman8991@gmail.com>
2022-07-03 16:12:52 +0600

    dropdown button responsive done

--- src/Components/SearchScrean/SearchScrean.js
+++ src/Components/SearchScrean/SearchScrean.js
@@ -39,27 +39,27 @@ const SearchScrean = () => {
     return (
         <>
             <div className='stratigy_bg'>
-                <div className='d-flex flex-column justify-content-center align-items-center py-5 my-3'>
-                    <div className='my-3 '>
-                        <select className='px-3 py-2 bg-light mx-3 select-border' name="" id="">
-                            <option value="" selected>{t('grade')}</option>
-                            <option value="">data</option>
-                            <option value="">data</option>
-                            <option value="">data</option>
-                        </select>
-                        <select className='px-3 py-2 bg-light mx-3' name="" id="">
+                <div className='d-flex flex-column justify-content-center align-items-center py-5 my-5'>
+                    <div className='my-3'>
+                        <select className='px-md-3 px-1 py-md-2 bg-light mx-md-3 select-border' name="" id="">
                             <option value="" selected>{t('subject')}</option>
                             <option value="">data</option>
                             <option value="">data</option>
                             <option value="">data</option>
                         </select>
-                        <select className='px-3 py-2 bg-light mx-3' name="" id="">
+                        <select className='px-md-3 px-1 py-md-2 bg-light mx-2 mx-md-3 select-border ' name="" id="">
+                            <option value="" selected>{t('grade')}</option>
+                            <option value="">data</option>
+                            <option value="">data</option>
+                            <option value="">data</option>
+                        </select>
+                        <select className='px-md-3 px-1 py-md-2 bg-light mx-md-3 select-border' name="" id="">
                             <option value="" selected>{t('topic')}</option>
                             <option value="">data</option>
                             <option value="">data</option>
                             <option value="">data</option>
                         </select>
-                        <select className='d-none d-md-inline px-3 py-2 bg-light mx-3' name="" id="">
+                        <select className='d-none d-md-inline px-1  px-md-3 py-md-2 bg-light mx-md-3 select-border' name="" id="">
                             <option value="" selected>{t('skill')}</option>
                             <option value="">data</option>
                             <option value="">data</option>
@@ -67,19 +67,19 @@ const SearchScrean = () => {
                         </select>
                     </div>
                     <div>
-                        <select className='d-inline d-md-none px-3 py-2 bg-light mx-3' name="" id="">
+                        <select className='d-inline d-md-none px-1  px-md-3 py-md-2 bg-light mx-md-3 select-border' name="" id="">
                             <option value="" selected>{t('skill')}</option>
                             <option value="">data</option>
                             <option value="">data</option>
                             <option value="">data</option>
                         </select>
-                        <select className='px-3 py-2 bg-light mx-3' name="" id="">
+                        <select className=' px-1 px-md-3 py-md-2 bg-light mx-2 mx-md-3 select-border' name="" id="">
                             <option value="" selected>{t('sub_topic')}</option>
                             <option value="">data</option>
                             <option value="">data</option>
                             <option value="">data</option>
                         </select>
-                        <select className='px-3 py-2 bg-light mx-3' name="" id="">
+                        <select className=' px-1 px-md-3 py-md-2 bg-light mx-md-3 select-border' name="" id="">
                             <option value="" selected>{t('sub_sub_topic')}</option>
                             <option value="">data</option>
                             <option value="">data</option>
