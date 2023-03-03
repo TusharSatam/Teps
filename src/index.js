851984456107da64757ca1b47c7911868e515848
Azhar Sayyed <azhartsiddiqui@gmail.com>
2023-03-03 12:04:55 +0530

    Drop Down issue solve

--- src/index.js
+++ src/index.js
@@ -1,40 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import AuthProvider from './Context/AuthContext';
-import './i18nextInit'
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import AuthProvider from "./Context/AuthContext";
+import "./i18nextInit";
+import { BrowserRouter } from "react-router-dom";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import axios from 'axios';
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import axios from "axios";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 document.addEventListener("DOMContentLoaded", () => {
   const start = new Date().getTime();
   window.addEventListener("beforeunload", () => {
     const end = new Date().getTime();
-    const totalTime = (end - start) / 1000
+    const totalTime = (end - start) / 1000;
     const data = {
       date: Date.now(),
       time: totalTime,
-      user_id: JSON.parse(localStorage.getItem("data"))._id
-    }
-    axios.post(`${process.env.REACT_APP_BASE_URL}pulledStr/averageTime`, data)
+      user_id: JSON.parse(localStorage.getItem("data"))._id,
+    };
+    axios.post(`${process.env.REACT_APP_BASE_URL}pulledStr/averageTime`, data);
   });
 });
 root.render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId="750670617713-ui98njvoppd8evq323752skbaok6lr10.apps.googleusercontent.com">
-
+  <>
+    <GoogleOAuthProvider clientId={process.env.CLIENT_ID}>
       <AuthProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </AuthProvider>
     </GoogleOAuthProvider>
-  </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
